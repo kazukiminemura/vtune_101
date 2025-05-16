e9e0167d771ed9088f0ea00b38b3f8d656f2072b
kazuki minemura <kazuki.minemura@gmail.com>
2025-05-16 12:42:46 +0900

    Update bandwith_cache_test.cpp

--- example/memory_cache_profile/bandwith_cache_test.cpp
+++ example/memory_cache_profile/bandwith_cache_test.cpp
@@ -5,16 +5,16 @@
 
 constexpr size_t dataSize = 1024 * 1024 * 1000; // 10000M floats
 constexpr size_t num_threads = 112;
-constexpr size_t chunk_size = dataSize / num_threads; // 9.8M items per thread
+constexpr size_t chunk_size = dataSize / num_threads;
 
-void access_memory(float* data, size_t offset, size_t size) {
+void access_memory(char* data, size_t offset, size_t size) {
     for (size_t i = 0; i < size; ++i) {
-        data[offset + i] *= 2.0f;
+        data[offset + i] *= 2;
     }
 }
 
 int main() {
-    std::vector<float> data(dataSize, 1.0f);
+    std::vector<char> data(dataSize, 1);
     std::vector<std::thread> threads;
 
     auto start = std::chrono::high_resolution_clock::now();
